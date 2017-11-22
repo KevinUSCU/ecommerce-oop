9c73dec0b733ec0cf1866d87e1cb56453b10142d
Wes Reid <bwreid@gmail.com>
2017-11-22 13:12:26 -0800

    Add bug fix

--- scripts/models/ClickListener.js
+++ scripts/models/ClickListener.js
@@ -2,7 +2,7 @@ class ClickListener {
   constructor () {}
 
   static add (item, callback, preventDefault=true) {
-    document.querySelector(item).addEventListener('click', (event) => {
+    item.addEventListener('click', (event) => {
       if (preventDefault) event.preventDefault()
       callback()
     })
